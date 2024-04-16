2c2964ac4f41fa22c1de2a6da42d4920df979c9c
Jaroslaw Polowy <jarekpolowy@gmail.com>
2024-04-16 11:34:39 +0100

    BDP-6: schedule cron job to run every hour

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from database.database import (
     create_brms_table,
 )
+from utils.cron_manager import set_cron_job
 from utils.data_handler import handle_received_data
 from utils.date_utils import get_date_range
 
 NUMBER_OF_DAYS_TO_PULL = 2
 
-create_brms_table()
 
-dates = get_date_range(NUMBER_OF_DAYS_TO_PULL)
-for date in dates:
-    handle_received_data(date)
+def main() -> None:
+    create_brms_table()
+
+    dates = get_date_range(NUMBER_OF_DAYS_TO_PULL)
+    for date in dates:
+        handle_received_data(date)
+
+    set_cron_job()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/cron_manager.py
@@ -0,0 +1,17 @@
+import os
+from crontab import CronTab
+
+
+def set_cron_job() -> None:
+    cron = CronTab(user=True)
+    script_path = os.path.abspath(os.path.dirname(__file__))
+    main_script_path = os.path.join(script_path, "..", "main.py")
+
+    job = cron.new(command=f"python3 {main_script_path}")
+
+    job.minute.on(0)
+    job.hour.every(1)
+
+    cron_file_path = os.path.expanduser("~/.crontab.txt")
+
+    cron.write_to_user(cron_file_path)
