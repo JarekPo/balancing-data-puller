1db5a850ddecdb59f8f4cd84668a554d5e211343
Jaroslaw Polowy <jarekpolowy@gmail.com>
2024-04-17 12:44:49 +0100

    update cron settings

--- utils/cron_manager.py
+++ utils/cron_manager.py
@@ -1,20 +1,31 @@
 import os
 from crontab import CronTab
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def set_cron_job() -> None:
-    cron = CronTab(user=True)
-    script_path = os.path.abspath(os.path.dirname(__file__))
-    main_script_path = os.path.join(script_path, "..", "main.py")
+    try:
+        cron = CronTab(user=True)
+        script_path = os.path.abspath(os.path.dirname(__file__))
+        main_script_path = os.path.join(script_path, "..", "main.py")
 
-    job = cron.new(command=f"python3 {main_script_path}")
+        job = cron.new(command=f"python3 {main_script_path}")
 
-    job.minute.on(0)
-    job.hour.every(1)
+        job.minute.on(0)
+        job.hour.every(1)
 
-    cron_file_path = "/root/.crontab.txt"
-    cron.write(cron_file_path)
+        cron_file_path = "/root/.crontab.txt"
+        cron.write(cron_file_path)
 
-    os.chmod(cron_file_path, 0o600)
+        os.chmod(cron_file_path, 0o600)
 
-    os.system(f"crontab -u root {cron_file_path}")
+        os.system(f"crontab -u root {cron_file_path}")
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    set_cron_job()
