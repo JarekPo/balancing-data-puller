e3c98d932ac600acfd352250dd351ebe340d58a1
Jaroslaw Polowy <jarekpolowy@gmail.com>
2024-04-17 12:18:45 +0100

    update cron settings

--- utils/cron_manager.py
+++ utils/cron_manager.py
@@ -3,7 +3,7 @@ from crontab import CronTab
 
 
 def set_cron_job() -> None:
-    cron = CronTab(user=True)
+    cron = CronTab(user=False)
     script_path = os.path.abspath(os.path.dirname(__file__))
     main_script_path = os.path.join(script_path, "..", "main.py")
 
@@ -13,5 +13,4 @@ def set_cron_job() -> None:
     job.hour.every(1)
 
     cron_file_path = os.path.expanduser("~/.crontab.txt")
-
     cron.write_to_user(cron_file_path)
